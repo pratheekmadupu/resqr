13f024abb84c2d6739a3abd9c8f040e467cb4f33
pratheekmadupu <pratheekmadupu2006@gmail.com>
2026-02-28 23:06:42 +0530

    switch to BrowserRouter and add vercel.json for clean URLs on custom domain

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <App />
             <Toaster position="top-center" reverseOrder={false} />
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>,
 )
